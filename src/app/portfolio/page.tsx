32ab7cdeefbb70708d177f1458eddeeb9923107f
Vitaliy Morzhov <voxfoot@gmail.com>
2023-07-11 23:32:16 +0500

    portfolio begin work

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from './Portfolio.module.scss';
 
 const Potfolio = () => (
-  <div>Potfolio</div>
+  <div className={styles.container}>
+    <h1 className={styles.selectItem}>Chose a gallery</h1>
+    <div className={styles.item}>
+      <Link href="/portfolio/illustrations">
+        <span>Illustrations</span>
+      </Link>
+    </div>
+  </div>
 );
 
 export default Potfolio;
